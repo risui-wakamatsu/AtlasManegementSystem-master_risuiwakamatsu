216da5b1c88c09503785499f4acb862ed7bd6690
若松利水 <risui1212@gmail.com>
2023-12-02 16:40:39 +0900

    20231202リレーション更新

--- app/Models/Users/Subjects.php
+++ app/Models/Users/Subjects.php
@@ -15,7 +15,10 @@ class Subjects extends Model
         'subject'
     ];
 
-    public function users(){
-        return;// リレーションの定義
+    public function users()
+    { // リレーションの定義
+        return $this->belongsToMany(User::class, 'subject_users', 'subject_id', 'user_id');
+        //(使用するモデル,使用するテーブル,リレーション元のidを入れた中間テーブルのカラム,リレーション先のidを入れた中間テーブルのカラム)
+
     }
 }
